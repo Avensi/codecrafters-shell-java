33e0ccf74d9fe88097fa897835b30a41adcc404b
Avensi <helenali2000@hotmail.com>
2026-02-10 19:51:15 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
         while (true){
             System.out.print("$ ");
-            Scanner scanner = new Scanner(System.in);
             String command = scanner.nextLine();
+
             if(command.equals("exit")){
-                break;
+                System.exit(0);
             } else if(command.startsWith("echo")){
-                System.out.println(command.replace("echo", "").trim());
+                System.out.println(command.substring(5));
             } else {
                 System.out.println(command + ": command not found");
             }
