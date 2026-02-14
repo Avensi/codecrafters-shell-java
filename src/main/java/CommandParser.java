16371cdb50669c44f041e3293d8d71c2d17b83a2
Avensi <helenali2000@hotmail.com>
2026-02-15 00:28:06 +0100

    codecrafters submit [skip ci]

--- src/main/java/CommandParser.java
+++ src/main/java/CommandParser.java
@@ -7,7 +7,8 @@ public class CommandParser {
     private static final char DOUBLE_QUOTE = '\"';
     private static final char SPACE = ' ';
     private static final char ESCAPE = '\\';
-
+    private static final char DOLLAR = '$';
+    private static final char NEWLINE = '\n';
     public List<String> parseCommand(String input){
         List<String> result = new ArrayList<>();
         boolean inSingleQuote = false;
@@ -17,13 +18,29 @@ public class CommandParser {
         for(int i=0; i<input.length(); i++){
             char currentChar = input.charAt(i);
             if (currentChar == ESCAPE && !inSingleQuote){
-                if (i + 1 < input.length()) {
-                    char nextChar = input.charAt(i + 1);
-                    currentWord.append(nextChar);
-                    i++;
+                if(inDoubleQuote){
+                    if (i + 1 < input.length()) {
+                        char nextChar = input.charAt(i + 1);
+                        if (nextChar == DOUBLE_QUOTE || nextChar == ESCAPE || nextChar ==  DOLLAR|| nextChar == NEWLINE) {
+                            currentWord.append(nextChar);
+                            i++;
+                        } else {
+                            currentWord.append(currentChar);
+                        }
+
+                    } else {
+                        currentWord.append(currentChar);
+                    }
                 } else {
-                    currentWord.append(currentChar);
+                    if (i + 1 < input.length()) {
+                        char nextChar = input.charAt(i + 1);
+                        currentWord.append(nextChar);
+                        i++;
+                    } else {
+                        currentWord.append(currentChar);
+                    }
                 }
+
             } else {
                 if (currentChar == SINGLE_QUOTE && !inDoubleQuote){
                     inSingleQuote = !inSingleQuote;
