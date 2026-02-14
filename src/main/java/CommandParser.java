ecacba0a925a2719934d3bfabf79612f8844e08e
Avensi <helenali2000@hotmail.com>
2026-02-14 16:10:00 +0100

    codecrafters submit [skip ci]

--- src/main/java/CommandParser.java
+++ src/main/java/CommandParser.java
@@ -3,26 +3,41 @@ import java.util.List;
 
 public class CommandParser {
 
+    private static final char SINGLE_QUOTE = '\'';
+    private static final char DOUBLE_QUOTE = '\"';
+    private static final char SPACE = ' ';
+    private static final char ESCAPE = '\\';
+
     public List<String> parseCommand(String input){
         List<String> result = new ArrayList<>();
         boolean inSingleQuote = false;
         boolean inDoubleQuote = false;
         StringBuilder currentWord = new StringBuilder();
-        for (char c: input.toCharArray()){
-            if (c == '\'' && !inDoubleQuote){
-                inSingleQuote = !inSingleQuote;
-            }
-            else if (c == '\"' && !inSingleQuote){
-                inDoubleQuote = !inDoubleQuote;
-            }
-            else if (c == ' ' && !inSingleQuote && !inDoubleQuote){
-                if (!currentWord.isEmpty()){
-                    result.add(currentWord.toString());
-                    currentWord.setLength(0);
+
+        for(int i=0; i<input.length(); i++){
+            char currentChar = input.charAt(i);
+            if (currentChar == ESCAPE){
+                if (i + 1 < input.length()) {
+                    char nextChar = input.charAt(i + 1);
+                    currentWord.append(nextChar);
                 }
             } else {
-                currentWord.append(c);
+                if (currentChar == SINGLE_QUOTE && !inDoubleQuote){
+                    inSingleQuote = !inSingleQuote;
+                }
+                else if (currentChar == DOUBLE_QUOTE && !inSingleQuote){
+                    inDoubleQuote = !inDoubleQuote;
+                }
+                else if (currentChar == SPACE && !inSingleQuote && !inDoubleQuote){
+                    if (!currentWord.isEmpty()){
+                        result.add(currentWord.toString());
+                        currentWord.setLength(0);
+                    }
+                } else {
+                    currentWord.append(currentChar);
+                }
             }
+
         }
         if(!currentWord.isEmpty()){
             result.add(currentWord.toString());
