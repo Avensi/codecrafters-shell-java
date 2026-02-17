2cefba311aef71756b4cb3057de1d04d30fd5c5d
Avensi <helenali2000@hotmail.com>
2026-02-17 23:40:40 +0100

    codecrafters submit [skip ci]

---
+++ src/main/java/CommandDispatcher.java
@@ -0,0 +1,44 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommandDispatcher {
+
+    private final Command commandService;
+    private final CommandParser commandParser;
+
+    PrintStream originalOut = System.out;
+
+    public CommandDispatcher(Command commandService, CommandParser commandParser){
+        this.commandService = commandService;
+        this.commandParser = commandParser;
+    }
+
+    public void dispatch(String input) throws FileNotFoundException {
+        String fileName = null;
+
+        List<String> tokens = commandParser.parseCommand(input);
+        String commandName = tokens.getFirst();
+
+        int redirectIndex = commandParser.getRedirectIndex(tokens);
+
+        if (redirectIndex != -1){
+            fileName = tokens.get(redirectIndex + 1);
+            tokens = new ArrayList<>(tokens.subList(0, redirectIndex));;
+            FileOutputStream outputFile = new FileOutputStream(fileName);
+            System.setOut(new PrintStream(outputFile));
+        }
+
+        switch (commandName) {
+            case "exit" -> System.exit(0);
+            case "echo" -> commandService.echo(tokens);
+            case "type" -> commandService.type(tokens);
+            case "pwd" -> commandService.pwd();
+            case "cd" -> commandService.cd(tokens);
+            default -> commandService.execute(tokens, fileName);
+        }
+        System.setOut(originalOut);
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,39 +7,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        Command commandService = new Command();
-        CommandParser commandParser = new CommandParser();
-        PrintStream originalOut = System.out;
+        CommandDispatcher commandDispatcher = new CommandDispatcher(new Command(), new CommandParser());
 
         while (true){
             System.out.print("$ ");
             String input = scanner.nextLine().trim();
-            String fileName = null;
-
             if (input.isEmpty()) {
                 continue;
             }
-            List<String> tokens = commandParser.parseCommand(input);
-            String commandName = tokens.getFirst();
-            int redirectIndex = commandParser.getRedirectIndex(tokens);
-
-            if (redirectIndex != -1){
-                fileName = tokens.get(redirectIndex + 1);
-                tokens = new ArrayList<>(tokens.subList(0, redirectIndex));;
-                FileOutputStream outputFile = new FileOutputStream(fileName);
-                System.setOut(new PrintStream(outputFile));
-            }
-
-            switch (commandName) {
-                case "exit" -> System.exit(0);
-                case "echo" -> commandService.echo(tokens);
-                case "type" -> commandService.type(tokens);
-                case "pwd" -> commandService.pwd();
-                case "cd" -> commandService.cd(tokens);
-                default -> commandService.execute(tokens, fileName);
-            }
-            System.setOut(originalOut);
-
+            commandDispatcher.dispatch(input);
         }
     }
 }
