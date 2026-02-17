b7bb9bc6c6a2955f8be9feb62bbce8fb7c1db943
Avensi <helenali2000@hotmail.com>
2026-02-17 23:53:16 +0100

    codecrafters submit [skip ci]

--- src/main/java/CommandDispatcher.java
+++ src/main/java/CommandDispatcher.java
@@ -9,36 +9,41 @@ public class CommandDispatcher {
     private final Command commandService;
     private final CommandParser commandParser;
 
-    PrintStream originalOut = System.out;
-
     public CommandDispatcher(Command commandService, CommandParser commandParser){
         this.commandService = commandService;
         this.commandParser = commandParser;
     }
 
-    public void dispatch(String input) throws FileNotFoundException {
+    public void dispatch(String input) {
         String fileName = null;
+        PrintStream originalOut = System.out;
 
         List<String> tokens = commandParser.parseCommand(input);
         String commandName = tokens.getFirst();
 
         int redirectIndex = commandParser.getRedirectIndex(tokens);
 
-        if (redirectIndex != -1){
-            fileName = tokens.get(redirectIndex + 1);
-            tokens = new ArrayList<>(tokens.subList(0, redirectIndex));;
-            FileOutputStream outputFile = new FileOutputStream(fileName);
-            System.setOut(new PrintStream(outputFile));
+        try {
+            if (redirectIndex != -1){
+                fileName = tokens.get(redirectIndex + 1);
+                tokens = new ArrayList<>(tokens.subList(0, redirectIndex));;
+                FileOutputStream outputFile = new FileOutputStream(fileName);
+                System.setOut(new PrintStream(outputFile));
+            }
+
+            switch (commandName) {
+                case "exit" -> System.exit(0);
+                case "echo" -> commandService.echo(tokens);
+                case "type" -> commandService.type(tokens);
+                case "pwd" -> commandService.pwd();
+                case "cd" -> commandService.cd(tokens);
+                default -> commandService.execute(tokens, fileName);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("No such file or directory");
+        } finally {
+            System.setOut(originalOut);
         }
 
-        switch (commandName) {
-            case "exit" -> System.exit(0);
-            case "echo" -> commandService.echo(tokens);
-            case "type" -> commandService.type(tokens);
-            case "pwd" -> commandService.pwd();
-            case "cd" -> commandService.cd(tokens);
-            default -> commandService.execute(tokens, fileName);
-        }
-        System.setOut(originalOut);
     }
 }
