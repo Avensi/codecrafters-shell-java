3077ff41ddcb737196962cbb790849da8fa2a53a
Avensi <helenali2000@hotmail.com>
2026-02-21 23:02:15 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,7 +25,6 @@ public class Main {
                 .parser(parser)
                 .completer(aggregateCompleter)
                 .option(LineReader.Option.AUTO_MENU, false)
-                .option(LineReader.Option.INSERT_TAB, true)
                 .option(LineReader.Option.AUTO_LIST, true)
                 .option(LineReader.Option.LIST_AMBIGUOUS, true)
                 .build();
